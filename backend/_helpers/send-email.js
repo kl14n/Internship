ca8258aa5001b80ed5666e42193aae81b53fef31
Khan <47729520+kl14n@users.noreply.github.com>
2020-09-14 13:59:12 +0700

    Big update

---
+++ backend/_helpers/send-email.js
@@ -0,0 +1,19 @@
+const nodemailer = require('nodemailer');
+const config = require('config.json');
+
+module.exports = sendEmail;
+
+async function sendEmail({
+    to,
+    subject,
+    html,
+    from = config.emailFrom
+}) {
+    const transporter = nodemailer.createTransport(config.smtpOptions);
+    await transporter.sendMail({
+        from,
+        to,
+        subject,
+        html
+    });
+}
